7dfdf482e4148668c8c42f080fbc3f6fa187420d
JumpingPistachio <JumpingPistachio@gmail.com>
2023-06-16 14:02:38 -0400

    Began working on framework core functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use std::process::{Command, ExitStatus};
+use std::io::Result;
+
+pub fn win_cmd(args: &[&str]) -> Result<ExitStatus> {
+    Command::new("cmd")
+        .args(args)
+        .spawn()?.wait()
+}
 fn main() {
-    println!("Hello, world!");
+    let _output = win_cmd(&["/k", "ping google.com"]);
 }
